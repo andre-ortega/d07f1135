400a785ef9a6745bab1ed2e1daec33b032b620b0
sansantas <ortegaan@oregonstate.edu>
2021-12-12 14:40:41 -0800

    iterations

--- gitConfs.sh
+++ gitConfs.sh
@@ -3,6 +3,5 @@ str="'$*'"
 gitCmd="git commit -m ${str}"
 eval "$gitCmd"
 value=`cat ~/Documents/notes/git.txt`
-echo ${value}
 cmd="git push https://SanSantas:${value}@github.com/SanSantas/BackupConfigs.git"
 eval "$cmd"
