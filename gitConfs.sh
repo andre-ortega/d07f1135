7ea71926a450cb733d688a39fe202b9a1590b4ea
sansantas <ortegaan@oregonstate.edu>
2021-12-12 13:40:11 -0800

    make gitConfs.sh executable

--- gitConfs.sh
+++ gitConfs.sh
@@ -2,5 +2,5 @@ git add -A
 git commit -m "Updated."
 value=`cat ~/Documents/notes/git.txt`
 echo ${value}
-cmd="git push https://sansantas:${value}@github.com/sansantas/BackupConfigs.git"
+cmd="git push https://SanSantas:${value}@github.com/SanSantas/BackupConfigs.git"
 eval "$cmd"
